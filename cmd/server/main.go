25ee22a93d539f5e3a6085593a55158e18ac72d7
Ryan Tarnowski <ryan.tarnowski@gmail.com>
2026-05-28 07:59:00 -0600

    setup user input loop on server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	"log"
-	"os"
-	"os/signal"
+	//"os"
+	//"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -29,20 +30,56 @@ func main() {
 		log.Fatalf("Error opening new channel %v", err)
 	}
 
-	err = pubsub.PublishJSON(newChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
+	gamelogic.PrintServerHelp()
 
-	if err != nil {
-		log.Printf("could not publish: %v", err)
+MainLoop:
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		switch userInput[0] {
+		case "pause":
+			fmt.Println("Publishing paused game state")
+			err = pubsub.PublishJSON(
+				newChannel,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish: %v", err)
+			}
+		case "resume":
+			fmt.Println("Publishing resumes game state")
+			err = pubsub.PublishJSON(
+				newChannel,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish: %v", err)
+			}
+		case "quit":
+			fmt.Println("Goodbye")
+			break MainLoop
+		case "help":
+			fmt.Println("help")
+			gamelogic.PrintServerHelp()
+		default:
+			fmt.Println("Unknown command")
+		}
 	}
 
-	fmt.Println("Pause message sent!")
-
 	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	sig := <-signalChan
+	//signalChan := make(chan os.Signal, 1)
+	//signal.Notify(signalChan, os.Interrupt)
+	//sig := <-signalChan
 
-	fmt.Printf("\nReceived signal: %s. Exiting...\n", sig)
+	//fmt.Printf("\nReceived signal: %s. Exiting...\n", sig)
 }

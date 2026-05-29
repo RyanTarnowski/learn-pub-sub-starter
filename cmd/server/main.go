0d2fe2c43632594dea75c3381f2a4d46ca3ac112
Ryan Tarnowski <ryan.tarnowski@gmail.com>
2026-05-29 07:08:34 -0600

    setup game logs queue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,6 +30,18 @@ func main() {
 		log.Fatalf("Error opening new channel %v", err)
 	}
 
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("Error in declare and bind: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 
 	for {
